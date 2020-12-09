94d5ed679e99f96d75799516613f55b92b2e9f06
qybit <2601970213@qq.com>
2020-12-09 10:12:38 +0800

    solved 62 on 2020/12/9 a.m.

---
+++ 0062-Unique-Paths/src/Solution.java
@@ -0,0 +1,25 @@
+/**
+ * @author qybit
+ * @create 2020-12-09 10:04
+ * @Since 1.0.0
+ */
+public class Solution {
+
+    public int uniquePaths(int m, int n) {
+        int[][] f = new int[m][n];
+
+        for (int i = 0; i < m; i++) {
+            f[i][0] = 1;
+            // System.out.println(f[i][0]);
+        }
+        for (int i = 0; i < n; i++) {
+            f[0][i] = 1;
+        }
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j < n; j++) {
+                f[i][j] = f[i-1][j] + f[i][j-1];
+            }
+        }
+        return f[m-1][n-1];
+    }
+}
